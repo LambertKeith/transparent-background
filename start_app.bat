4d4bb6943e4c743ebf6f257d38f3aec95afe9b0b
keith <1>
2024-04-10 18:03:04 +0800

    1

--- start_app.bat
+++ start_app.bat
@@ -1,2 +1,6 @@
+python -m venv .venv
 call .venv\Scripts\activate.bat
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 uvicorn app:app --host 0.0.0.0 --port 8000 
